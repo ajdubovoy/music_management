8f8e551e7e0aff6a9c6d20ff6b587c19ff17a377
Alexander Dubovoy <ajdubovoy@gmail.com>
2020-07-14 14:51:33 +0200

    get auth working without sign up

--- lib/music_management_web/router.ex
+++ lib/music_management_web/router.ex
@@ -48,8 +48,8 @@ defmodule MusicManagementWeb.Router do
   scope "/", MusicManagementWeb do
     pipe_through [:browser, :redirect_if_user_is_authenticated]
 
-    get "/users/register", UserRegistrationController, :new
-    post "/users/register", UserRegistrationController, :create
+    # get "/users/register", UserRegistrationController, :new
+    # post "/users/register", UserRegistrationController, :create
     get "/users/log_in", UserSessionController, :new
     post "/users/log_in", UserSessionController, :create
     get "/users/reset_password", UserResetPasswordController, :new
